f4524260d4cc7eee705163fe20b75b384607dbd3
DESKTOP-T0R8L53\denys <denyspetrychenko@gmail.com>
2024-10-07 15:50:14 +0200

    Actualización y arreglo de error en el tipo de dato Int a int. El programa del ejercicio 1 ahora funciona

--- ejercicios_denys/src/main/java/org/example/Bateria_ejercicios_sencillos.java
+++ ejercicios_denys/src/main/java/org/example/Bateria_ejercicios_sencillos.java
@@ -6,7 +6,7 @@ public class Bateria_ejercicios_sencillos {
     public void ejercicio1(){
         System.out.println("Hola, Bienvenido, Introduce un número: "); //EL SISTEMA MUESTRA QUE INTRODUZCAMOS NÚMERO
         Scanner myScanner = new Scanner(System.in); //DEFINICIÓN VARIABLE SCANNER
-        Int numero = myScanner.nextInt(); //RECOGIDA DE DATO INTRODUCIDO POR EL USUARIO COMO UN NÚMERO ENTERO EN LA VARIABLE numero
+        int numero = myScanner.nextInt(); //RECOGIDA DE DATO INTRODUCIDO POR EL USUARIO COMO UN NÚMERO ENTERO EN LA VARIABLE numero
 
         if (numero < 0){
             System.out.println("El número es menor que 0");
